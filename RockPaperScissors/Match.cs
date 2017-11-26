752ba4b43b3ae0c57980eb06d3894e77e5477c0c
Michael Okarimia <git@michaelokarimia.com>
2017-11-26 14:07:41 +0000

    Test for Match state

---
+++ RockPaperScissors/Match.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    public class Match
+    {
+        private Player playerOne;
+        private Player playerTwo;
+
+        public Match(Player playerOne, Player playerTwo)
+        {
+            this.playerOne = playerOne;
+            this.playerTwo = playerTwo;
+        }
+
+        public Result PlayGames(List<Game> games)
+        {
+            var drawCount = 0;
+            var p1WinCount = 0;
+            var p2WinCount = 0;
+
+            foreach(Game game in games)
+            {
+                var result = game.Play();
+                if (result == Result.Draw)
+                    drawCount++;
+                if (result == Result.PlayerOneWins)
+                    p1WinCount++;
+                if (result == Result.PlayerTwoWins)
+                    p2WinCount++;
+            }
+
+            if (p1WinCount > p2WinCount)
+                return Result.PlayerOneWins;
+            if (p2WinCount > p1WinCount)
+                return Result.PlayerTwoWins;
+
+            return Result.Draw;
+        }
+    }
+}
---
+++ Tests/MatchTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RockPaperScissors;
+
+namespace Tests
+{
+    [TestFixture]
+    public class MatchTests
+    {
+        Match match; 
+
+
+        [Test]
+        public void PlayerOneWinsTwoGamesAndMatch()
+        {
+            var playerOne = new Player();
+            var playerTwo = new Player();
+
+            match = new Match(playerOne, playerTwo);
+
+            Game gameOne = new Game(playerOne.Play(Move.Rock), playerTwo.Play(Move.Scissors));
+            Game gameTwo = new Game(playerOne.Play(Move.Scissors), playerTwo.Play(Move.Paper));
+            Game gameThree = new Game(playerOne.Play(Move.Paper), playerTwo.Play(Move.Rock));
+
+            var gamesPlayed = new List<Game> { gameOne, gameTwo, gameThree };
+
+           
+
+
+            Assert.AreEqual(Result.PlayerOneWins, match.PlayGames(gamesPlayed));
+        }
+    }
+}
