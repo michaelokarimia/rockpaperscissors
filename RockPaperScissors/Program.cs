d40030b3f7a7088021c5f269db091c5700128b07
Michael Okarimia <git@michaelokarimia.com>
2017-11-26 18:07:17 +0000

    WIP needs extracting into a round class

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -12,14 +12,42 @@ namespace RockPaperScissors
         {
             Console.WriteLine("Select (r)ock, (p)aper or (s)cissors as your move");
 
-            var p1string = Console.ReadLine().ToLowerInvariant().Trim();
+            var inputString = Console.ReadLine().ToLowerInvariant().Trim();
 
-            HumanPlayer human = new HumanPlayer();
+            IPlayer playerOne = new HumanPlayer();
 
-            if (MoveValidator.IsValid(p1string))
-                human.Play(MoveValidator.Parse(p1string));
+            if (MoveValidator.IsValid(inputString))
+                playerOne.Play(MoveValidator.Parse(inputString));
             else
                 Console.WriteLine("Invalid input, Select(r)ock, (p)aper or(s)cissors as your move");
+
+            Console.WriteLine("Select (r)ock, (p)aper or (s)cissors as your move");
+
+            inputString = Console.ReadLine().ToLowerInvariant().Trim();
+
+            IPlayer playerTwo = new HumanPlayer();
+
+            if (MoveValidator.IsValid(inputString))
+                playerTwo.Play(MoveValidator.Parse(inputString));
+            else
+                Console.WriteLine("Invalid input, Select(r)ock, (p)aper or(s)cissors as your move");
+
+
+            var gameCount = 0;
+
+            Console.WriteLine("Playing game");
+
+            var firstGame = new Game(playerOne, playerTwo);
+
+            var firstResult = firstGame.Play();
+            gameCount++;
+
+            Console.WriteLine("Game {0}:", gameCount);
+
+            if (firstResult == Result.Draw)
+                Console.WriteLine("Result was a draw");
+            else
+                Console.WriteLine("Player {0} won", firstResult == Result.PlayerOneWins ? "PlayerOne" : "PlayerTwo");
 
             Console.ReadKey();
         }
