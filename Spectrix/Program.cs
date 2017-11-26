1e1fe2f0ae913933049889b7dd711b624f2ee875
Michael Okarimia <git@michaelokarimia.com>
2017-11-26 12:18:09 +0000

    Add project files.

---
+++ Spectrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Spectrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
