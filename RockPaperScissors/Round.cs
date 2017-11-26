89b4c4422f16c7eb566789dc79564e444b538463
Michael Okarimia <git@michaelokarimia.com>
2017-11-26 13:43:17 +0000

    All Round scenarios pass tests
    Loads of if statements
    Uses test cases

--- RockPaperScissors/Round.cs
+++ RockPaperScissors/Round.cs
@@ -1,12 +1,9 @@
-﻿using System;
-
-namespace Tests
+﻿namespace Tests
 {
     public class Round
     {
         private Player p1;
         private Player p2;
-
 
         public Round(Player p1, Player p2)
         {
@@ -14,41 +11,37 @@ namespace Tests
             this.p2 = p2;
         }
 
-
         public Result Play()
         {
             // same moves will always result in a draw
             if (p1.LastMove == p2.LastMove)
                 return Result.Draw;
 
-
             //rock beats scissors, p1 wins
             if (p1.LastMove == Move.Rock && p2.LastMove == Move.Scissors)
                 return Result.PlayerOneWins;
 
             //rock loses against paper
-
-
-
-
+            if (p1.LastMove == Move.Rock && p2.LastMove == Move.Paper)
+                return Result.PlayerTwoWins;
 
             //scissors beats paper
-
             if (p1.LastMove == Move.Scissors && p2.LastMove == Move.Paper)
                 return Result.PlayerOneWins;
 
-
-
+            //scissor lose agaist rock
+            if (p1.LastMove == Move.Scissors && p2.LastMove == Move.Rock)
+                return Result.PlayerTwoWins;
 
             //paper loses against Scissors
-
             if (p1.LastMove == Move.Paper && p2.LastMove == Move.Scissors)
                 return Result.PlayerTwoWins;
 
+            //paper beats rock
+            if (p1.LastMove == Move.Paper && p2.LastMove == Move.Rock)
+                return Result.PlayerOneWins;
 
             return Result.Draw;
         }
-
-
     }
 }
---
+++ Tests/RoundTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class RoundTests
+    {
+
+        [TestCase(Move.Rock, Move.Scissors, Result.PlayerOneWins)]
+        [TestCase(Move.Rock, Move.Paper, Result.PlayerTwoWins)]
+        [TestCase(Move.Rock, Move.Rock, Result.Draw)]
+
+        [TestCase(Move.Paper, Move.Rock, Result.PlayerOneWins)]
+        [TestCase(Move.Paper, Move.Scissors, Result.PlayerTwoWins)]
+        [TestCase(Move.Paper, Move.Paper, Result.Draw)]
+
+        [TestCase(Move.Scissors, Move.Paper, Result.PlayerOneWins)]
+        [TestCase(Move.Scissors, Move.Rock, Result.PlayerTwoWins)]
+        [TestCase(Move.Scissors, Move.Scissors, Result.Draw)]
+        public void PlayerRoundRetunsExpectedResults(Move p1Move, Move p2Move, Result expectedResult)
+        {
+
+            Player player1 = new Player();
+            Player player2 = new Player();
+            var round = new Round(player1.Play(p1Move), player2.Play(p2Move));
+
+            Assert.AreEqual(expectedResult, round.Play());
+
+        }
+
+    }
+}
